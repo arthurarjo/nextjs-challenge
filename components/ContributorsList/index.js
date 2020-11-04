4806c863606b7e1bf764b5f0e798601a6ff76472
Arthur Araujo <aaraujjo@outlook.com.br>
2020-11-04 17:49:52 -0300

    setup modal in contributorslist

--- components/ContributorsList/index.js
+++ components/ContributorsList/index.js
@@ -1,12 +1,30 @@
-import React from "react";
-import { useRouter } from "next/router";
-
+import React, { useState } from "react";
+import axios from "axios";
 import styles from "./ContributorsList.module.css";
 
 import Title from "../Title";
 import ContributorsCard from "../ContributorsCard";
+import Modal from "../Modal";
 
 const ContributorsList = ({ contributors }) => {
+  const [modal, setModal] = useState(false);
+  const [modalLoading, setModalLoading] = useState(false);
+  const [repositories, setRepositories] = useState([]);
+
+  async function seeContributorsRepo(username) {
+    setModal(true);
+    setModalLoading(true);
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?sort=updated`
+    );
+    setRepositories(res.data);
+    setModalLoading(false);
+  }
+
+  function hideModal() {
+    setModal(false);
+  }
+
   return (
     <div>
       <Title>Top Contributors</Title>
@@ -14,10 +32,30 @@ const ContributorsList = ({ contributors }) => {
       <div className={styles.grid}>
         {contributors.map((contributor) => {
           return (
-            <ContributorsCard key={contributor.id} contributor={contributor} />
+            <ContributorsCard
+              key={contributor.id}
+              contributor={contributor}
+              seeContributorsRepo={seeContributorsRepo}
+            />
           );
         })}
       </div>
+
+      <Modal show={modal} handleClose={hideModal}>
+        {modalLoading && <p className="center">loading repositories</p>}
+        <ul>
+          {!modalLoading &&
+            repositories.map((repo) => {
+              return (
+                <li>
+                  {repo.name} - {repo.fork ? "it is forked" : "not forked"} -{" "}
+                  {repo.stargazers_count} stars -{" "}
+                  {new Date(repo.updated_at).toLocaleDateString()}
+                </li>
+              );
+            })}
+        </ul>
+      </Modal>
     </div>
   );
 };
